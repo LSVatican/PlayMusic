8363669664d95e257592a310be7fa50c47b79ad3
LSVatican <malikalfirdausyogi@gmail.com>
2026-05-07 11:59:30 +0700

    update script.js

--- script.js
+++ script.js
@@ -229,8 +229,12 @@ function updateMediaMetadata(song) {
             artist: 'Daus XD',
             album: 'PlayMusic',
             artwork: [
-                { src: 'favicon.ico', sizes: '96x96', type: 'image/png' },
-                { src: 'favicon.ico', sizes: '512x512', type: 'image/png' }
+                { src: 'favicon.ico', sizes: '96x96', type: 'image/x-icon' },
+                { src: '/Favicon.png', sizes: '128x128', type: 'image/png' },
+                { src: '/Favicon.png', sizes: '192x192', type: 'image/png' },
+                { src: '/Favicon.png', sizes: '256x256', type: 'image/png' },
+                { src: '/Favicon.png', sizes: '384x384', type: 'image/png' },
+                { src: '/Favicon.png', sizes: '512x512', type: 'image/png' }
             ]
         });
     }
